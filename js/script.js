d59231b1fc16ec29f8dfa170f45c620bf810dcb4
default <yunus.emre.guzel@mths.ca>
2025-06-08 21:48:53 +0000

    delted the semis

--- js/script.js
+++ js/script.js
@@ -9,15 +9,15 @@
 // Function to get tech news from NewsData.io
 async function newsAPI() {
   try {
-    const url = 'https://newsdata.io/api/1/latest?apikey=pub_c0774b64c35e44e8a862750fbeda4ea8&q=technology%20';
-    const result = await fetch(url);
-    const jsonData = await result.json();
+    const url = 'https://newsdata.io/api/1/latest?apikey=pub_c0774b64c35e44e8a862750fbeda4ea8&q=technology%20'
+    const result = await fetch(url)
+    const jsonData = await result.json()
 
     // Show the title of the first news article
-    const firstTitle = jsonData.results[0].title;
-    document.getElementById("result").innerHTML = firstTitle;
+    const firstTitle = jsonData.results[0].title
+    document.getElementById("result").innerHTML = firstTitle
 
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
