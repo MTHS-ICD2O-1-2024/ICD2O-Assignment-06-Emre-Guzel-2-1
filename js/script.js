3d94cb75d2eb261e2ece394ecd9a39eba6fc395b
default <yunus.emre.guzel@mths.ca>
2025-06-08 22:27:04 +0000

    finshed the code adn added descrioptoins

--- js/script.js
+++ js/script.js
@@ -5,18 +5,20 @@
 // This file contains the JS functions for index.html
 
 "use strict"
-
 // Function to get tech news from NewsData.io
 async function newsAPI() {
   try {
-    const url = 'https://newsdata.io/api/1/latest?apikey=pub_c0774b64c35e44e8a862750fbeda4ea8&q=technology%20'
+    const url = 'https://newsdata.io/api/1/latest?apikey=pub_c0774b64c35e44e8a862750fbeda4ea8&q=technology'
     const result = await fetch(url)
     const jsonData = await result.json()
 
-    // Show the title of the first news article
+    // Show the title and description of the first news article
     const firstTitle = jsonData.results[0].title
-    document.getElementById("result").innerHTML = firstTitle
-
+    const description = jsonData.results[0].description
+    document.getElementById("result").innerHTML = `
+      <h2>${firstTitle}</h2>
+      <p>${description}</p>
+    `
   } catch (error) {
     console.log(error)
   }
