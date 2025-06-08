73610f70be543ee0b10c3e7ac6835743450c4396
default <yunus.emre.guzel@mths.ca>
2025-06-08 21:48:08 +0000

    finshed the backend

--- js/script.js
+++ js/script.js
@@ -1,32 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Emre Guzel
-// Created on: May 20 2025
+// Created on: June 8 2025
 // This file contains the JS functions for index.html
 
 "use strict"
 
-// Setting the function 
+// Function to get tech news from NewsData.io
 async function newsAPI() {
-
-  // Setting the weather API 
   try {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=f5cce8e3a6c922f1fdf3bd14085cd28e';
+    const url = 'https://newsdata.io/api/1/latest?apikey=pub_c0774b64c35e44e8a862750fbeda4ea8&q=technology%20';
     const result = await fetch(url);
     const jsonData = await result.json();
 
-    const temp = jsonData.main.temp;
-    const iconCode = jsonData.weather[0].icon;
-
-    // Setting the weather ICON API
-    const iconUrl = 'https://openweathermap.org/img/wn/' + iconCode + '@2x.png';
-    document.getElementById('weatherImage').src = iconUrl;
-
-    // Calculations
-    document.getElementById("result").innerHTML = (temp - 273.15).toFixed(0) + "" + "°C";
+    // Show the title of the first news article
+    const firstTitle = jsonData.results[0].title;
+    document.getElementById("result").innerHTML = firstTitle;
 
   } catch (error) {
     console.log(error);
   }
-
 }
